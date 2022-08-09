fb3c98fa4176b710dc7f32159aad82f541298362
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-10 00:45:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -303,7 +303,7 @@
 						else if(this.y >= 21*road         && this.y < 22*road        ) {this.dby = 18; this.tlu = 21*road; this.tld = 22*road;}
 						else 								this.dby = 0;
 
-						//alert("num:"+this.num+" dbx:"+this.dbx+" dby:"+this.dby+" st:"+this.status+" turn:"+this.turn+" n:"+this.n);
+						alert("num:"+this.num+" dbx:"+this.dbx+" dby:"+this.dby+" st:"+this.status+" turn:"+this.turn+" n:"+this.n);
 
 						//set status if not turning
 						// 1 up  2 right  3 down  4 left
