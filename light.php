6e346949eb7155f14dc0696bec373a45cc01e179
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-20 20:56:19 +0900

    Update light.php

--- light.php
+++ light.php
@@ -6,7 +6,46 @@
 		<table border = "1">
 			<tr>
 				<td>
-					<table border = "1">
+					<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>1</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				</td>
+				<td>
+					<table>
 						<tr>
 							<td></td>
 							<td>↰</td>
@@ -24,7 +63,7 @@
 						<tr>
 							<td>↞</td>
 							<td></td>
-							<td></td>
+							<td>2</td>
 							<td></td>
 							<td>↠</td>
 						</tr>
@@ -44,8 +83,45 @@
 						</tr>
 					</table>
 				</td>
-				<td>2</td>
-				<td>3</td>
+				<td>
+					<table>
+						<tr>
+							<td></td>
+							<td>↰</td>
+							<td>↟</td>
+							<td>↱</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>↻</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>⤴</td>
+						</tr>
+						<tr>
+							<td>↞</td>
+							<td></td>
+							<td>3</td>
+							<td></td>
+							<td>↠</td>
+						</tr>
+						<tr>
+							<td>↶</td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td>⤵</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>↲</td>
+							<td>↡</td>
+							<td>↳</td>
+							<td></td>
+						</tr>
+					</table>
+				</td>
 			</tr>
 			<tr>
 				<td>1</td>
