26ad98f2fa064413cec345be0d499109a6a9c98b
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-20 21:30:38 +0900

    Update light.php

--- light.php
+++ light.php
@@ -49,37 +49,37 @@
 					<table>
 						<tr>
 							<td></td>
-							<td>↰</td>
-							<td>↟</td>
-							<td>↱</td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
 							<td></td>
 						</tr>
 						<tr>
-							<td>↻</td>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
 							<td></td>
 							<td></td>
 							<td></td>
-							<td>⤴</td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
 						</tr>
 						<tr>
-							<td>↞</td>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
 							<td></td>
 							<td>2</td>
 							<td></td>
-							<td>↠</td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
 						</tr>
 						<tr>
-							<td>↶</td>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
 							<td></td>
 							<td></td>
 							<td></td>
-							<td>⤵</td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
 						</tr>
 						<tr>
 							<td></td>
-							<td>↲</td>
-							<td>↡</td>
-							<td>↳</td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
 							<td></td>
 						</tr>
 					</table>
@@ -88,56 +88,380 @@
 					<table>
 						<tr>
 							<td></td>
-							<td>↰</td>
-							<td>↟</td>
-							<td>↱</td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
 							<td></td>
 						</tr>
 						<tr>
-							<td>↻</td>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
 							<td></td>
 							<td></td>
 							<td></td>
-							<td>⤴</td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
 						</tr>
 						<tr>
-							<td>↞</td>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
 							<td></td>
 							<td>3</td>
 							<td></td>
-							<td>↠</td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
 						</tr>
 						<tr>
-							<td>↶</td>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
 							<td></td>
 							<td></td>
 							<td></td>
-							<td>⤵</td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
 						</tr>
 						<tr>
 							<td></td>
-							<td>↲</td>
-							<td>↡</td>
-							<td>↳</td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
 							<td></td>
 						</tr>
 					</table>
 				</td>
 			</tr>
 			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>4</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>5</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>6</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
 			</tr>
 			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>7</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>8</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>9</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
 			</tr>
 			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>10</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>11</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
+				<table>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↰' onclick='alert("up left 2")'/></td>
+							<td><input type='button' value='↟' onclick='alert("up up 3")'/></td>
+							<td><input type='button' value='↱' onclick='alert("up right 1")'/></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↻' onclick='alert("left up 11")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤴' onclick='alert("right up 4")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↞' onclick='alert("left left 12")'/></td>
+							<td></td>
+							<td>12</td>
+							<td></td>
+							<td><input type='button' value='↠' onclick='alert("right right 6")'/></td>
+						</tr>
+						<tr>
+							<td><input type='button' value='↶' onclick='alert("left down 10")'/></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td><input type='button' value='⤵' onclick='alert("right down 5")'/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type='button' value='↲' onclick='alert("down left 7")'/></td>
+							<td><input type='button' value='↡' onclick='alert("down down 9")'/></td>
+							<td><input type='button' value='↳' onclick='alert("down right 8")'/></td>
+							<td></td>
+						</tr>
+					</table>
 			</tr>
 		</table>				
 	</body>
