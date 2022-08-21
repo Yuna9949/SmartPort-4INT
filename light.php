a2bc457384181571862dfeaaae03f05a744b67d6
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-21 19:57:01 +0900

    Update light.php

--- light.php
+++ light.php
@@ -526,7 +526,7 @@
 				status("t10", contact["light10"]);
 				status("t11", contact["light11"]);
 				status("t12", contact["light12"]);
-				alert(contact["light12"]+"
+				alert(contact["light12"]+"O");
 			});
 			
 		}
