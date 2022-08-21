92d9cf751853956de8e8ef9231aad6b4df63aa78
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-21 23:54:02 +0900

    Update light.php

--- light.php
+++ light.php
@@ -19,7 +19,7 @@
 				</td>
 			</tr>
 			<tr>
-				<td colspan="3" id="outputt" width="500px" style="word-break:break-all">t</td>
+				<td colspan="3" id="outputt" width="500px" style="word-break:break-all"></td>
 			</tr>
 			<tr>
 				<td><input type='button' value='mysql_conn' onclick='mysql_conn()'/></td>
