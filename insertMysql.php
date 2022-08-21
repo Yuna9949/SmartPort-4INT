9a8b050711adf8361e4483dc009ef7097583cc2a
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-21 21:24:19 +0900

    Update insertMysql.php

--- insertMysql.php
+++ insertMysql.php
@@ -22,7 +22,8 @@
       ) VALUES (
         '$t1', '$t2', '$t3',
         '$t4', '$t5', '$t6',
-        '$t7', '$t8', '$t9', NOW()
+        '$t7', '$t8', '$t9',
+        '$t10', '$t11', '$t12', NOW()
       );";
     mysqli_query($conn, $sql);
     mysqli_close($conn);
