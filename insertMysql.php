e83d6d0b953e51003c7b886c1b12aabd980493e5
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-21 21:09:31 +0900

    Update insertMysql.php

--- insertMysql.php
+++ insertMysql.php
@@ -14,7 +14,7 @@
     $t12 = $_GET['t12'];
     
     $result['success'] = true;
-    $result['data'] = $t1+" "+$t2+" "+$t3+" "+$t4+" "+$t5+" "+$t6+" "+$t7+" "+$t8+" "+$t9+" "+$t10+" "+$t11+" "+$t12;
+    $result['data'] = "success";
     
   } catch(exception $e) {
     $result['success'] = false;
