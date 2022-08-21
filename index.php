8f01e53630613e19625aa6069b26ef96703ce8a6
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-21 21:40:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -405,7 +405,7 @@
 							if(this.turn == 5 || this.turn == 9 || this.turn == 10)	this.status = 3;
 							if(this.turn == 2 || this.turn == 7 || this.turn == 12)	this.status = 4;
 						}
-						var ud = (this.tlu+this.tld)/+2;
+						var ud = (this.tlu+this.tld)/+5;
 						var lr = (this.tll+this.tlr)/2;
 						
 						var span = 2;
