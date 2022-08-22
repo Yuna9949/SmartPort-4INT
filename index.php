1bb84dc2137bf2879743f1f58183863ce4c2941b
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-22 18:19:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -406,7 +406,7 @@
 							check = 9003;
 							this.carrying += this.speed;
 						}
-						else if(this.crain == 3 && this.x < 10*road+wth/2 && this.y > 2*road-15) {
+						else if(this.crain == 3 && this.x < 10*road+wth/2 && this.y+this.speed > 2*road-15) {
 							this.status = 1;
 							this.turn = 0;
 							check = 90031;
