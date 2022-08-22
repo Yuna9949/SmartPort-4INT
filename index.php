6fe2bc596fa64eaf4e76ce8b1800b58dc948dd33
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-22 18:09:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -338,7 +338,7 @@
 						var check = 0;
 						//set start position
 						if(this.dbx == 9 && this.crain == 4) {
-							alert(this.y+" "+(2*road-15)+" "+)
+							alert(this.y+" "+(2*road-15)+" "+(this.y-2*road+15))
 							if(this.y-this.speed < 2*road-15) {
 								this.status = 4;
 							}
