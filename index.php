7071f7b404c6326bb6e02584c90899788607c591
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-22 18:05:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -339,13 +339,19 @@
 						//set start position
 						if(this.dbx == 9 && this.crain == 4) {
 							alert(this.y)
-							if(this.y < 2*road-15)	this.status = 4;
+							if(this.y-this.speed < 2*road-15) {
+								this.status = 4;
+								this.y += (this.speed - (this.y-2*road+15));
+							}
 							else 				this.status = 1;
 							this.turn = 0;
 							check = 904;
 						}
 						else if(this.dbx == 9 && this.crain != 0) {
-							if(this.y < 3*road-15)	this.status = 4;
+							if(this.y-this.speed < 3*road-15) {
+								this.status = 4;
+								this.y += (this.speed - (this.y-3*road+15));
+							}
 							else 			this.status = 1;
 							this.turn = 0;
 							check = 903;
