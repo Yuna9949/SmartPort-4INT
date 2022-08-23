e9005d3de2da33ea02259942a997123e37c5940f
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-23 16:02:25 +0900

    Update getCrainMySql.php

--- getCrainMySql.php
+++ getCrainMySql.php
@@ -1,18 +1,18 @@
 <?php
-    $num = $_GET['num'];
+	$num = $_GET['num'];
 
-		$conn = mysqli_connect("localhost","root","smartport4int","test");
+	$conn = mysqli_connect("localhost","root","smartport4int","test");
 
-		$sql = "SELECT * FROM crain".$num." ORDER BY num ASC WHERE loaded = 0";
-		$result = mysqli_query($conn, $sql);
+	$sql = "SELECT * FROM crain".$num." ORDER BY num ASC WHERE loaded = 0";
+	$result = mysqli_query($conn, $sql);
 
-		$row = mysqli_fetch_assoc($result);
-		$result = $row['carry'];
-    		$time = $row['time'];
+	$row = mysqli_fetch_assoc($result);
+	$result = $row['carry'];
+    	$time = $row['time'];
 
-    		$sql = "UPDATE crain".$num." SET loaded = 1 WHERE time = ".$time;
-    		mysqli_query($conn, $sql);
+    	$sql = "UPDATE crain".$num." SET loaded = 1 WHERE time = ".$time;
+    	mysqli_query($conn, $sql);
    
-		mysqli_close($conn);
-		echo json_encode($result, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+	mysqli_close($conn);
+	echo json_encode($result, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 ?>
