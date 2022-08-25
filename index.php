6112b65d96990747a5a95cfed19bcb6d1eec182c
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-25 18:11:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -644,15 +644,15 @@
 						
 						//unloading carry
 						// yellowgreen 2
-						if(this.carry == 5 && this.dbx == 3 && this.y > 9*road && this.y < 11*road) {
+						if(this.carry == 5 && this.dbx == 3 && this.y > 9*road && this.y < 11*road-16) {
 							
 							if(this.y < 11*road-16) 	this.status = 3;
 							else 				this.status = 2;
 							this.turn = 0;
-							check = 238;
-							
-							//alert(this.num+" : "+(9*road)+" "+this.y+" "+(11*road)+" "+this.status);
-							
+							check = 238;							
+						}
+						if(this.carry == 5 && this.dbx == 3 && this.y < 11*road-16 && this.y+this.speed > 11*road-16) {
+							alert(this.num+" "+this.y+" "+(11*road-16));
 						}
 						// skyblue 3
 						// green 4
