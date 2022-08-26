fe60ee011360275bf26065190699206ba45279fa
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-26 12:52:05 +0900

    Update SPTS.php

--- SPTS.php
+++ SPTS.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, height=device-height,
-                   minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0">
     
     <title>SPTS</title>
     
