4121822b87b3ea202c5eb1d60d10313f2a34d9c7
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-08-26 12:32:19 +0900

    Create SPTS.php

---
+++ SPTS.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, height=device-height,
+                   minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0">
+    <title>SPTS</title>
+    <style>
+    </style>
+    <script type="text/javascript">
+    </script>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td colspan="2" rowspan="5" width="70%">
+          <div>
+            <canvas id="canvas" width="100" height="100"></canvas>
+          </div>
+          <br>
+          <br>
+        </td>
+        <th>
+          <br>
+          Smart Port Traffic Simulator
+        </th>
+      </tr>
+      <tr>
+        <td>
+          <div>
+            <div>
+            </div>
+            <select>
+            </select>
+          </div>
+        </td>
+      </tr>
+  </body>
+</html>
