6d2c01b876784e63e22bd08484e5ef3f3c74731f
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-09-01 15:21:26 +0900

    Create getCarMysql.php

---
+++ getCarMysql.php
@@ -0,0 +1,27 @@
+<?php
+	try{
+		$num = (string)$_GET['num'];
+
+		$conn = mysqli_connect("localhost","root","smartport4int","test");
+
+		$sql = "SELECT * FROM truck WHERE num=".$num." ORDER BY time DESC;";
+		$result = mysqli_query($conn, $sql);
+
+		$row = mysqli_fetch_assoc($result);
+		$data['x'] = $row['x'];
+    $data['y'] = $row['y'];
+   
+		mysqli_close($conn);
+		
+		$data['success'] = true;
+		$data['msg'] = $time;
+		$data['code'] = $num;
+	} catch(exception $e) {
+		$data['success'] = false;
+    		$data['x'] = "fail";
+    		$data['msg'] = $e->getMessage();
+    		$data['code']	= $e->getCode();
+	} finally {
+		echo json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+	}
+?>
