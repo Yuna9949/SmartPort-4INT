07f640471aaa2f5161525314c8088722dc61c980
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-09-20 02:59:44 +0900

    Update getRoute.php

--- getRoute.php
+++ getRoute.php
@@ -7,7 +7,7 @@
 	$start_traffic = 2;//$_GET['start_traffic'];
 	$start_enter = 4;//$_GET['start_enter'];
 	$dest_traffic = 5;//$_GET['dest_traffic'];
-	$dest_enter = 3;//$_GET['dest_enter'];
+	$dest_enter = 1;//$_GET['dest_enter'];
 	$conn = mysqli_connect("localhost","root","smartport4int","test");
 
 	$sql = "SELECT * FROM map WHERE start_traffic='".$start_traffic."' AND start_enter='".$start_enter."';";
@@ -15,24 +15,29 @@
 	$result = mysqli_query($conn, $sql);
 
 	$maxcost = 0;
-	$traffic = 0;
+	$traffic = 10;
+	$dt = 2;
 
 	while($row = mysqli_fetch_assoc($result)){
 		//print_r($row);
 		echo ''.$row['dest_traffic'].' / '.$row['dest_enter'].'';
 
-		$cost = getCost($row['$dest_traffic'], $row['$dest_enter'], $dest_traffic, $dest_enter);
+		$cost = getCost($row['dest_traffic'], $row['dest_enter'], $dest_traffic, $dest_enter);
 
-		echo ' / cost='.$cost;
+		echo ' / cost='.$cost.' / ';
 		if($cost > $maxcost) {
 			$maxcost = $cost;
 			$traffic = $row['traffic'];
+			$dt = $row['dest_traffic'];
 		}
 	}
-	/*
+
 	$sql = "SELECT * FROM traffic ORDER BY time DESC";
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_assoc($result);
+	
+	if($dt > 9) $row["light".$dt] = $traffic;
+	else	    $row["light0".$dt] = $traffic;
 
 	$sql = "INSERT INTO traffic (
 			light01, light02, light03, 
@@ -44,7 +49,7 @@
 			'".$row["light04"]."', '".$row["light05"]."', '".$row["light06"]."',
 			'".$row["light07"]."', '".$row["light08"]."', '".$row["light09"]."',
 			'".$row["light10"]."', '".$row["light11"]."', '".$row["light12"]."', NOW()
-		);";*/
+		);";
 
 	mysqli_close($conn);
 	
