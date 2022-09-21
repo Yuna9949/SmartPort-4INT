ca28040c1c3267fe92b56c97433f9aaac1db50aa
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-09-21 21:44:03 +0900

    Update resetDB.php

--- resetDB.php
+++ resetDB.php
@@ -1,6 +1,19 @@
 <?php
 	$conn = mysqli_connect("localhost","root","smartport4int","test");
 
+	$sql = "INSERT INTO traffic (
+        		light01, light02, light03, 
+        		light04, light05, light06, 
+        		light07, light08, light09, 
+        		light10, light11, light12, time
+      		) VALUES (
+        		'0', '0', '0',
+        		'0', '0', '0',
+        		'0', '0', '0',
+        		'0', '0', '0', NOW()
+      		);";
+	$result = mysqli_query($conn, $sql);
+
 	$sql = "UPDATE map SET weight='1';";
 	$result = mysqli_query($conn, $sql);
 
