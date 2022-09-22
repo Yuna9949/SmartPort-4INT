f637ab316854894a24ce6e28965e7174207df224
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-09-22 23:01:54 +0900

    Create insertStop.php

---
+++ insertStop.php
@@ -0,0 +1,47 @@
+<?php
+	$number = $_GET['number'];
+	$traffic_light = $_GET['traffic_light'];
+
+	$conn = mysqli_connect("localhost","root","smartport4int","test");
+
+	// update traffic
+	$sql = "SELECT * FROM traffic ORDER BY time DESC";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	
+	$traffic = 0;
+
+	if($traffic_light > 9) 	$traffic = $row["light".$traffic_light];
+	else	    		$traffic = $row["light0".$traffic_light];
+	
+	if($traffic == 3 || $traffic == 4 || $traffic == 11 || $traffic == 13 || $traffic == 17)
+		$traffic = 21;
+	else if($traffic == 1 || $traffic == 6 || $traffic == 8 || $traffic == 15)
+		$traffic == 22;
+	else if($traffic == 5 || $traffic == 9 || $traffic == 10 || $traffic == 14 || $traffic == 16)
+		$traffic == 23;
+	else if($traffic == 2 || $traffic == 7 || $traffic == 12 || $traffic == 18)
+		$traffic == 24;
+
+	if($traffic_light > 9) 	$row["light".$traffic_light] = $traffic;
+	else	    		$row["light0".$traffic_light] = $traffic;
+
+	$sql = "INSERT INTO traffic (
+			light01, light02, light03, 
+			light04, light05, light06, 
+			light07, light08, light09, 
+			light10, light11, light12, time
+		) VALUES (
+			'".$row["light01"]."', '".$row["light02"]."', '".$row["light03"]."',
+			'".$row["light04"]."', '".$row["light05"]."', '".$row["light06"]."',
+			'".$row["light07"]."', '".$row["light08"]."', '".$row["light09"]."',
+			'".$row["light10"]."', '".$row["light11"]."', '".$row["light12"]."', NOW()
+		);";
+	//echo ' / '.$sql.' / ';
+	$result = mysqli_query($conn, $sql);
+
+	mysqli_close($conn);
+
+	$data = $number;
+	echo $data;
+?>
