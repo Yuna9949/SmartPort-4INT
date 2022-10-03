ac809b5ab5216716f4fc7048efb5e10e9ee948c6
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-10-03 20:21:28 +0900

    Update updateWeight.php

--- updateWeight.php
+++ updateWeight.php
@@ -2,22 +2,18 @@
 	$traffic_light = $_GET['traffic_light'];
 	$traffic_enter = $_GET['traffic_enter'];
 
-	if($traffic_light != 0) {
-
-		$conn = mysqli_connect("localhost","root","smartport4int","test");
-		$sql = "SELECT * FROM map 
-			WHERE dest_traffic='".$traffic_light."' 
-			AND dest_enter='".$traffic_enter."'";
-	
+	$conn = mysqli_connect("localhost","root","smartport4int","test");
+	$sql = "SELECT * FROM map 
+		WHERE dest_traffic='".$traffic_light."' 
+		AND dest_enter='".$traffic_enter."'";
 		$result = mysqli_query($conn, $sql);
-		while($row = mysqli_fetch_assoc($result)){
-			$row['weight'] = $row['weight'] + 500;
-			$sql2 = "UPDATE map SET weight='".$row['weight']."' WHERE description='".$row['description']."';";
-			mysqli_query($conn, $sql2);
-		}
-			
-		mysqli_query($conn, $sql);
+	while($row = mysqli_fetch_assoc($result)){
+		$row['weight'] = $row['weight'] + 500;
+		$sql2 = "UPDATE map SET weight='".$row['weight']."' WHERE description='".$row['description']."';";
+		mysqli_query($conn, $sql2);
 	}
+		
+	mysqli_query($conn, $sql);
 
 	$data = $traffic_light;
 	echo $data;
