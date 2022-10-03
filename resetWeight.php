f42be75b2b2cf6b8b76ef5730606027763dba66e
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-10-03 22:11:20 +0900

    Create resetWeight.php

---
+++ resetWeight.php
@@ -0,0 +1,11 @@
+<?php
+	$traffic_light = $_GET['traffic_light'];
+	$traffic_enter = $_GET['traffic_enter'];
+
+	$conn = mysqli_connect("localhost","root","smartport4int","test");
+	$sql = "UPDATE map SET weight = weight + 100 WHERE dest_traffic='$traffic_light' AND dest_enter='$traffic_enter';";
+	mysqli_query($conn, $sql);
+
+	$data = $traffic_light;
+	echo $data;
+?>
