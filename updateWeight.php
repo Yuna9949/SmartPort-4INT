9c141b86c88523bdf0100d8869f1f7350227dab7
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-10-03 21:39:09 +0900

    Update updateWeight.php

--- updateWeight.php
+++ updateWeight.php
@@ -2,7 +2,7 @@
 	$traffic_light = $_GET['traffic_light'];
 	$traffic_enter = $_GET['traffic_enter'];
 
-	$sql = "UPDATE map SET weight = 500 WHERE dest_traffic='".$traffic_light."' AND dest_enter='".$traffic_enter."';";
+	$sql = "UPDATE map SET weight = '500' WHERE dest_traffic='$traffic_light' AND dest_enter='$traffic_enter';";
 	mysqli_query($conn, $sql);
 
 	$data = $traffic_light;
