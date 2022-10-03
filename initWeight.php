a2109f3a65851cf147e02eb85d5a23e310245532
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-10-03 18:41:24 +0900

    Update initWeight.php

--- initWeight.php
+++ initWeight.php
@@ -1,2 +1,53 @@
 <?php
+	$road = $_GET['road'];
+	$wth = $_GET['wth'];
+
+	$conn = mysqli_connect("localhost","root","smartport4int","test");
+
+	// update traffic
+	$sql = "SELECT * FROM get_traffic";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	
+	$traffic = 0;
+	$lst="";
+
+	if($traffic_light > 9) 	$lst="light".$traffic_light;
+	else	    		$lst="light0".$traffic_light;
+
+	$traffic = $row[$lst];
+	
+	if($traffic == 3 || $traffic == 4 || $traffic == 11 || $traffic == 13 || $traffic == 17)
+		$traffic = 21;
+	else if($traffic == 1 || $traffic == 6 || $traffic == 8 || $traffic == 15)
+		$traffic = 22;
+	else if($traffic == 5 || $traffic == 9 || $traffic == 10 || $traffic == 14 || $traffic == 16)
+		$traffic = 23;
+	else if($traffic == 2 || $traffic == 7 || $traffic == 12 || $traffic == 18)
+		$traffic = 24;
+
+	if($logcheck != 'false') {
+		$row[$lst] = $traffic;
+
+		$sql = "INSERT INTO traffic (
+				light01, light02, light03, 
+				light04, light05, light06, 
+				light07, light08, light09, 
+				light10, light11, light12, time
+			) VALUES (
+				'".$row["light01"]."', '".$row["light02"]."', '".$row["light03"]."',
+				'".$row["light04"]."', '".$row["light05"]."', '".$row["light06"]."',
+				'".$row["light07"]."', '".$row["light08"]."', '".$row["light09"]."',
+				'".$row["light10"]."', '".$row["light11"]."', '".$row["light12"]."', NOW()
+			);";
+		$result = mysqli_query($conn, $sql);
+	}
+
+	$sql = "UPDATE get_traffic SET ".$lst." = '".$traffic."'"; 
+	$result = mysqli_query($conn, $sql);
+
+	mysqli_close($conn);
+
+	$data = $traffic_light;
+	echo $data;
 ?>
