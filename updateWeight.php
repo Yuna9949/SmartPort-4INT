b8dfc88c4c00f55a38c55e467b67a4641e9c8de6
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-10-03 20:55:48 +0900

    Update updateWeight.php

--- updateWeight.php
+++ updateWeight.php
@@ -19,37 +19,7 @@
 		$result = mysqli_query($conn, $sql);
 	while($row = mysqli_fetch_assoc($result)){
 		
-		if($row['weight'] == $r2w+1500) 	$row['weight'] = $r2w+2000;
-		if($row['weight'] == $r3w+1500) 	$row['weight'] = $r3w+2000;
-		if($row['weight'] == $r4w+1500) 	$row['weight'] = $r4w+2000;
-		if($row['weight'] == $r3+1500) 		$row['weight'] = $r3+2000;
-		if($row['weight'] == $r4+1500) 		$row['weight'] = $r4+2000;
-		if($row['weight'] == $r6+1500) 		$row['weight'] = $r6+2000;
-		if($row['weight'] == $r7+1500) 		$row['weight'] = $r7+2000;
-		
-		if($row['weight'] == $r2w+1000) 	$row['weight'] = $r2w+1500;
-		if($row['weight'] == $r3w+1000) 	$row['weight'] = $r3w+1500;
-		if($row['weight'] == $r4w+1000) 	$row['weight'] = $r4w+1500;
-		if($row['weight'] == $r3+1000) 		$row['weight'] = $r3+1500;
-		if($row['weight'] == $r4+1000) 		$row['weight'] = $r4+1500;
-		if($row['weight'] == $r6+1000) 		$row['weight'] = $r6+1500;
-		if($row['weight'] == $r7+1000) 		$row['weight'] = $r7+1500;
-		
-		if($row['weight'] == $r2w+500) 		$row['weight'] = $r2w+1000;
-		if($row['weight'] == $r3w+500) 		$row['weight'] = $r3w+1000;
-		if($row['weight'] == $r4w+500) 		$row['weight'] = $r4w+1000;
-		if($row['weight'] == $r3+500) 		$row['weight'] = $r3+1000;
-		if($row['weight'] == $r4+500) 		$row['weight'] = $r4+1000;
-		if($row['weight'] == $r6+500) 		$row['weight'] = $r6+1000;
-		if($row['weight'] == $r7+500) 		$row['weight'] = $r7+1000;
-		
-		if($row['weight'] == $r2w) 		$row['weight'] = $r2w+500;
-		if($row['weight'] == $r3w) 		$row['weight'] = $r3w+500;
-		if($row['weight'] == $r4w) 		$row['weight'] = $r4w+500;
-		if($row['weight'] == $r3) 		$row['weight'] = $r3+500;
-		if($row['weight'] == $r4) 		$row['weight'] = $r4+500;
-		if($row['weight'] == $r6) 		$row['weight'] = $r6+500;
-		if($row['weight'] == $r7) 		$row['weight'] = $r7+500;
+		$row['weight'] = $row['weight']+100;
 		
 		$sql2 = "UPDATE map SET weight='".$row['weight']."' WHERE description='".$row['description']."';";
 		mysqli_query($conn, $sql2);
