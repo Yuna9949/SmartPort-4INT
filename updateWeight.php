bff7c05605b267e04403f47411f69e8534014e79
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-12-12 23:57:22 +0900

    Update updateWeight.php

--- updateWeight.php
+++ updateWeight.php
@@ -1,12 +1,11 @@
 <?php
 	$traffic_light = $_GET['traffic_light'];
 	$traffic_enter = $_GET['traffic_enter'];
-	$senario = $_GET['senario'];
 
 	$conn = mysqli_connect("localhost","root","smartport4int","test");
 	$sql = "UPDATE map SET weight = weight + 1000 WHERE dest_traffic='$traffic_light' AND dest_enter='$traffic_enter';";
 	mysqli_query($conn, $sql);
 
-	$data = $senario;
+	$data = $traffic_light;
 	echo $data;
 ?>
