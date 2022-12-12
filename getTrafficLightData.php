1103a9a09a48f952488491a87ae969f74d2fd090
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-12-13 01:46:07 +0900

    Update getTrafficLightData.php

--- getTrafficLightData.php
+++ getTrafficLightData.php
@@ -4,36 +4,36 @@
 	$sql = "SELECT * FROM `test_traffic_light`;";
 	$result = mysqli_query($conn, $sql);
 
-  $row11 = mysqli_fetch_assoc($result);
-  $row12 = mysqli_fetch_assoc($result);
-  $row13 = mysqli_fetch_assoc($result);
-  $row14 = mysqli_fetch_assoc($result);
-  $row21 = mysqli_fetch_assoc($result);
-  $row22 = mysqli_fetch_assoc($result);
-  $row23 = mysqli_fetch_assoc($result);
-  $row24 = mysqli_fetch_assoc($result);
+ 	$row11 = mysqli_fetch_assoc($result);
+  	$row12 = mysqli_fetch_assoc($result);
+  	$row13 = mysqli_fetch_assoc($result);
+  	$row14 = mysqli_fetch_assoc($result);
+  	$row21 = mysqli_fetch_assoc($result);
+  	$row22 = mysqli_fetch_assoc($result);
+  	$row23 = mysqli_fetch_assoc($result);
+  	$row24 = mysqli_fetch_assoc($result);
 
-  $car1l = 0;
-  $car1c = 0;
-  $car2l = 0;
-  $car2c = 0;
+  	$car1l = 0;
+  	$car1c = 0;
+  	$car2l = 0;
+  	$car2c = 0;
  
-  if     ($row11["detect_num"] == 1) {$car1l = 1; $car1c = 1;}
-  else if($row12["detect_num"] == 1) {$car1l = 1; $car1c = 2;}
-  else if($row13["detect_num"] == 1) {$car1l = 1; $car1c = 3;}
-  else if($row14["detect_num"] == 1) {$car1l = 1; $car1c = 4;}
+  	if     ($row11["detect_num"] == 1) {$car1l = 1; $car1c = 1;}
+  	else if($row12["detect_num"] == 1) {$car1l = 1; $car1c = 2;}
+  	else if($row13["detect_num"] == 1) {$car1l = 1; $car1c = 3;}
+  	else if($row14["detect_num"] == 1) {$car1l = 1; $car1c = 4;}
 
-  else if($row11["detect_num"] == 2) {$car2l = 2; $car2c = 1;}
-  else if($row12["detect_num"] == 2) {$car2l = 2; $car2c = 2;}
-  else if($row13["detect_num"] == 2) {$car2l = 2; $car2c = 3;}
-  else if($row14["detect_num"] == 2) {$car2l = 2; $car2c = 4;}
+  	else if($row11["detect_num"] == 2) {$car2l = 2; $car2c = 1;}
+  	else if($row12["detect_num"] == 2) {$car2l = 2; $car2c = 2;}
+  	else if($row13["detect_num"] == 2) {$car2l = 2; $car2c = 3;}
+  	else if($row14["detect_num"] == 2) {$car2l = 2; $car2c = 4;}
   
 	$data = json_encode(array(
-    "car1l" => $car1l,
-    "car1c" => $car1c,
-    "car2l" => $car2l,
-    "car2c" => $car2c));
+    		"car1l" => $car1l,
+    		"car1c" => $car1c,
+    		"car2l" => $car2l,
+    		"car2c" => $car2c));
+  	mysqli_free_result($result);
+  	mysqli_close($conn);
 	echo $data;
-  mysqli_free_result($result);
-  mysqli_close($conn);
 ?>
