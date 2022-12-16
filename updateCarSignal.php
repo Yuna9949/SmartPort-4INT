bdf614859e5bfb2d31d38657deb690f98b51e098
Yuna9949 <82620252+Yuna9949@users.noreply.github.com>
2022-12-16 20:08:57 +0900

    Update updateCarSignal.php

--- updateCarSignal.php
+++ updateCarSignal.php
@@ -1,9 +1,10 @@
 
 <?php
-	$traffic_signal = $_GET['traffic_signal'];=
+	$num = $_GET['num'];
+	$traffic_signal = $_GET['traffic_signal'];
                 
 	$conn = mysqli_connect("localhost","root","smartport4int","test");
-	$sql = "UPDATE car_get_signal SET car_signal='$traffic_signal';";
+	$sql = "UPDATE car_get_signal SET car_signal='$traffic_signal' WHERE num='$num';";
 	mysqli_query($conn, $sql);
 
 	$data = $traffic_signal;
